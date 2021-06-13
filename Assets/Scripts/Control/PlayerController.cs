675344cf2e6e6b64291634425f9550bc1e3615ab
Ali Aboshady <ali_aboshady@hotmail.com>
2021-06-13 21:48:24 +0100

    Player attack logic prototype

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -1,36 +1,56 @@
 using UnityEngine;
 using RPG.Movement;
+using RPG.Combat;
 
 namespace RPG.Control
 {
 	public class PlayerController : MonoBehaviour
 	{
 		Mover mover;
+		Fighter fighter;
 
 		private void Start()
 		{
 			mover = GetComponent<Mover>();
+			fighter = GetComponent<Fighter>();
 		}
 
 		private void Update()
 		{
-			if (Input.GetMouseButton(0))
-			{
-				MoveToCursor();
-			}
+			if (InteractWithCombat()) return;
+			if (InteractWithMovement()) return;
 		}
 
-		void MoveToCursor()
+		private bool InteractWithCombat()
 		{
-			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
+			foreach (RaycastHit hit in hits)
+			{
+				CombatTarget target = hit.transform.GetComponent<CombatTarget>();
+				if (target == null) continue;
 
+				if(Input.GetMouseButtonDown(0)) fighter.Attack(target);
+				return true;
+			}
+			return false;
+		}
+
+		private bool InteractWithMovement()
+		{	
 			RaycastHit hit;
-			bool hasHit = Physics.Raycast(ray, out hit);
+			bool hasHit = Physics.Raycast(GetMouseRay(), out hit);
 
 			if (hasHit)
 			{
-				mover.MoveTo(hit.point);
+				if(Input.GetMouseButton(0)) mover.MoveTo(hit.point);
+				return true;
 			}
+			return false;
+		}
+
+		private static Ray GetMouseRay()
+		{
+			return Camera.main.ScreenPointToRay(Input.mousePosition);
 		}
 	}
 }
