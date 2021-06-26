ddb129cabd141344cf161bc29776229a9184ae8f
LAPTOP-S5SHF46L\Ali <ali_aboshady@hotmail.com>
2021-06-26 03:03:23 +0100

    Entering a portal saves the scene as a checkpoint

--- Assets/Scripts/SceneManagement/SavingWrapper.cs
+++ Assets/Scripts/SceneManagement/SavingWrapper.cs
@@ -1,11 +1,21 @@
 using UnityEngine;
 using RPG.Saving;
+using System.Collections;
 
 namespace RPG.SceneManagement
 {
 	public class SavingWrapper : MonoBehaviour
 	{
 		const string defaultSaveFile = "save";
+		[SerializeField] float fadeInTime = 1f;
+
+		private IEnumerator Start()
+		{
+			Fader fader = FindObjectOfType<Fader>();
+			fader.FadeOutImmediate();
+			yield return GetComponent<SavingSystem>().LoadLastScene(defaultSaveFile);
+			yield return fader.FadeIn(fadeInTime);
+		}
 
 		private void Update()
 		{
